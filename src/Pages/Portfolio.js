763974e608896143582a2171ca4ccef7d193014e
Lauren Hutchinson <laurenhutchinson@Laurens-MacBook-Pro.local>
2021-03-01 13:19:07 -0800

    repo data api in the console

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./../App.css";
+import useFetch from "../utils/UseFetch";
+// import RepoList from "../RepoList/RepoList";
 
 const Portfolio = () => {
+  const { error, isPending, data: repos } = useFetch(
+    "https://api.github.com/users/lhutchinson22/repos"
+  );
+
   return (
     <>
       <div className="banner-image paraPort">
         <h1 className="banner-header">Portfolio</h1>
       </div>
-      <div className="container"></div>
+      <div className="container">
+        {error && <div>{error}</div>}
+        {isPending && <div>Loading...</div>}
+        {repos && repos.map((repo) => console.log(repo))}
+      </div>
     </>
   );
 };
---
+++ src/RepoList/RepoList.js
@@ -0,0 +1,20 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// const RepoList = ({ repos }) => {
+//   return (
+//     <div className="blog-list">
+//       {/* <h2>{title}</h2> */}
+//       {repos.map((repos) => (
+//         <div className="repo-preview" key={repo.id}>
+//           <Link to={`/repos/${repos.id}`}>
+//             <h2>{repos.title}</h2>
+//             {/* <p>written by: {repos.author}</p> */}
+//           </Link>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default RepoList;
