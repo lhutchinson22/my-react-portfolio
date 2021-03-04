5e888909a046380400070b74c35efee37282d8ef
Lauren Hutchinson <laurenhutchinson@Laurens-MacBook-Pro.local>
2021-03-03 17:21:06 -0800

    about me page done

--- client/src/Pages/About.js
+++ client/src/Pages/About.js
@@ -16,6 +16,22 @@ const About = () => {
             alt="this is me"
             style={{ width: "500px" }}
           />
+          <p id="aboutMeText">
+            Hey! Welcome to my site! My name is Lauren Hutchinson and after
+            moving home to the SF Bay Area from spending a couple of years
+            working as a Barrys Bootcamp fitness instructor in New York City, I
+            decided to go back to school for coding. Currently, I train clients
+            part-time, while full-time building new projects, learning new
+            coding languages, and constantly looking to grow as a web developer.
+            <br />
+            <br />
+            I'm so excited to begin my new coding career as I look for my first
+            developer job. When I'm not coding, I'm usually teaching myself how
+            to DJ, skateboarding, or hanging out with friends. Coding challenges
+            me in a new way and I'm grateful for this opportunity to expand.
+            Check out my portfolio and contact me about anything, I'd love to
+            connect with and learn from my fellow developers!
+          </p>
         </div>
       </div>
     </>
