a09e173cb9eb9b95868fb6699ca70ed7032d6a4f
Lauren Hutchinson <laurenhutchinson@Laurens-MacBook-Pro.local>
2021-03-12 15:18:33 -0800

    updated resume

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import resume from "./CompetitiveResumeUpdated.pdf";
+import resume from "./ResumeUpdated.pdf";
 
 const Footer = () => {
   return (
